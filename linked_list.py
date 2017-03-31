bd934f80213adcf33136f151d4a6170cb32c0f41
Sahil Thapar <sahilthapar1992@gmail.com>
2017-03-30 20:26:55 -0400

    Removing elements from linked list

--- linked_list.py
+++ linked_list.py
@@ -96,3 +96,27 @@ class Solution(object):
 
         node.val = node.next.val
         node.next = node.next.next
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        if not head:
+            return head
+        p = head
+        while p.next:
+            if p.next.val == val:
+                p.next = p.next.next
+            else:
+                p = p.next
+        return (head if head.val != val else head.next)
