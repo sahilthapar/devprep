df118d7d11fbff439a4fb2d780545b65d2fbedbd
Sahil Thapar <sahilthapar1992@gmail.com>
2017-03-30 21:18:22 -0400

    Added numbers represented as linkedlist

--- linked_list.py
+++ linked_list.py
@@ -148,3 +148,48 @@ class Solution(object):
             else:
                 p = p.next
         return head
+
+# Top k frequent elements in a list
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        return zip(*collections.Counter(nums).most_common(k))[0]
+
+# Addint two numbers
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        carry = 0
+        root = n = ListNode(0)
+        while (l1 or l2 or carry):
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+
+            digit = (v1 + v2 + carry) % 10
+            carry = (v1 + v2 + carry) / 10
+            print v1, v2, carry, digit
+            n.next = ListNode(digit)
+            n = n.next
+        return root.next
+            
