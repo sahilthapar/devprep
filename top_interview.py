dfdd1557d68e8e3c628688f2f7abd76d205a11ad
Sahil Thapar <sahilthapar1992@gmail.com>
2017-04-02 14:32:19 -0400

    Happy numbers

--- top_interview.py
+++ top_interview.py
@@ -108,4 +108,20 @@ class Solution(object):
         """
         return not(len(collections.Counter(nums)) == len(nums))
 
+
+# Happy numbers
+
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        seen = set()
+        while n != 1:
+            if n in seen:
+                return False
+            seen.add(n)
+            n = sum([int(c) ** 2 for c in str(n)])
+        return True
         
