f300d73ace7367a182b5e9a118b45cc876f05146
Sahil Thapar <sahilthapar1992@gmail.com>
2017-04-01 21:45:30 -0400

    Minimum stack implementation

--- top_interview.py
+++ top_interview.py
@@ -37,3 +37,54 @@ class Solution(object):
             leftDepth = self.maxDepth(root.left)
             rightDepth = self.maxDepth(root.right)
         return 1 + max(leftDepth, rightDepth)
+
+# Minimum stack implementation
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = list()
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        m = self.getMin()
+        if m is None or x < m:
+            m = x
+        self.stack.append((x, m))
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.stack.pop()
+
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][0]
+
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if len(self.stack) == 0:
+            return None
+        else:
+            return self.stack[-1][1]
